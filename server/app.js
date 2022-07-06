dba6b4d25194db7d5796e1fbbbeb60e593f4d3b3
Corey Breshears <coreybreshears619@gmail.com>
2022-07-06 13:34:35 -0400

    Redirect to index.html after configuring all other routes

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const db = require("./db");
 const AuthMiddleware = require("./middlewares/auth");
+const path = require("path");
 
 // create store for sessions to persist in database
 const sessionStore = new SequelizeStore({ db });
@@ -15,6 +16,9 @@ const { json, urlencoded } = express;
 const app = express();
 
 app.use(logger("dev"));
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "../client/build")));
+
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(express.static(join(__dirname, "public")));
@@ -38,6 +42,11 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.json({ error: err });
+});
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../client/build/index.html"));
 });
 
 module.exports = { app, sessionStore };
--- server/db/db.js
+++ server/db/db.js
@@ -2,6 +2,7 @@ const Sequelize = require("sequelize");
 
 const db = new Sequelize(
   process.env.DATABASE_URL ||
+    // "postgres://corey:password@localhost:5432/population",
     "postgres://aajmbffjgiwpkb:868aef96f0c4480c2b0e65c90accb969f28a6d67dfc93039b0b9c52e85f35b83@ec2-54-159-22-90.compute-1.amazonaws.com:5432/dbkolr8182255i",
   {
     logging: false,
